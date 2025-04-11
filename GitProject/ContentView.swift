a7798f575b2bcfdca5816df4cd9d958750d783b1
isabelpt <iaptny@gmail.com>
2025-04-11 12:42:27 -0400

    Initial Commit

---
+++ GitProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitProject
+//
+//  Created by Isabel Prado on 4/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitProject/GitProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitProjectApp.swift
+//  GitProject
+//
+//  Created by Isabel Prado on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct GitProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
